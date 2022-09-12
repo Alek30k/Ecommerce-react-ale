2f1ecf99890d86dcece787c648a01dd930a419d8
Alek30 <alemamk23k@gmail.com>
2022-09-11 22:59:08 -0300

    add products/categories

--- src/data.js
+++ src/data.js
@@ -47,7 +47,7 @@ export const popularProducts = [
   },
   {
     id: 2,
-    img: "https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+    img: "https://www.pngarts.com/files/5/Plain-Grey-T-Shirt-PNG-Picture.png",
   },
   {
     id: 3,
@@ -67,7 +67,7 @@ export const popularProducts = [
   },
   {
     id: 7,
-    img: "https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+    img: "https://www.pngarts.com/files/1/Ladies-Jacket-Transparent-Image.png",
   },
   {
     id: 8,
