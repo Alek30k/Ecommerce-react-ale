8750e6ff062be312b0a916c51a852fd5a568913a
Alek30 <alemamk23k@gmail.com>
2022-09-11 22:59:56 -0300

    add new section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,21 @@ import React from "react";
 import Announcement from "../components/Announcement";
 import Categories from "../components/Categories";
 import Navbar from "../components/Navbar";
+import Products from "../components/Products";
 import Slider from "../components/Slider";
+import Newsletter from "../components/Newsletter";
+import Footer from "../components/Footer";
 
 const Home = () => {
   return (
-    <div className="container">
+    <div>
       <Announcement />
       <Navbar />
       <Slider />
       <Categories />
+      <Products />
+      <Newsletter />
+      <Footer />
     </div>
   );
 };
