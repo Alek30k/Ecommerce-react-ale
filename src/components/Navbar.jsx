c8b110f20e730a8d3e07773e1b85177c21172825
Alek30 <alemamk23k@gmail.com>
2022-09-12 18:26:42 -0300

    add responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,9 +2,14 @@ import { Badge } from "@material-ui/core";
 import { Search, ShoppingCartOutlined } from "@material-ui/icons";
 import React from "react";
 import styled from "styled-components";
+import { mobile } from "../responsive";
+import { mobile2 } from "../responsive";
+import { mobile3 } from "../responsive";
 
 const Container = styled.div`
   height: 60px;
+  ${mobile({ height: "50px" })};
+  ${mobile2({ height: "55px" })}
 `;
 
 const Wrapper = styled.div`
@@ -12,6 +17,8 @@ const Wrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  ${mobile({ padding: "10px 0px" })}
+  ${mobile3({ padding: "10px 10px", marginRight: "20px" })}
 `;
 
 const Left = styled.div`
@@ -22,6 +29,8 @@ const Left = styled.div`
 const Language = styled.span`
   font-size: 14px;
   cursor: pointer;
+  ${mobile({ display: "none" })}
+  ${mobile2({ display: "none" })}
 `;
 
 const SearchContainer = styled.div`
@@ -34,6 +43,8 @@ const SearchContainer = styled.div`
 
 const Input = styled.input`
   border: none;
+  ${mobile({ width: "50px" })}
+  ${mobile2({ width: "50px" })}
 `;
 
 const Center = styled.div`
@@ -43,6 +54,9 @@ const Center = styled.div`
 
 const Logo = styled.h1`
   font-weight: bold;
+  ${mobile({ fontSize: "24px" })}
+  ${mobile2({ fontSize: "24px" })}
+  ${mobile3({ fontSize: "18px" })}
 `;
 
 const Right = styled.div`
@@ -50,12 +64,17 @@ const Right = styled.div`
   display: flex;
   align-items: center;
   justify-content: flex-end;
+  ${mobile({ flex: 2, justifyContent: "center" })}
+  ${mobile2({ flex: 2, justifyContent: "center" })}
 `;
 
 const MenuItem = styled.div`
   font-size: 14px;
   cursor: pointer;
   margin-left: 25px;
+  ${mobile({ fontSize: "10px", marginLeft: "10px" })}
+  ${mobile2({ fontSize: "12px", marginLeft: "10px" })}
+  ${mobile3({ fontSize: "10px", marginLeft: "10px" })}
 `;
 
 const Navbar = () => {
@@ -65,7 +84,7 @@ const Navbar = () => {
         <Left>
           <Language>EN</Language>
           <SearchContainer>
-            <Input />
+            <Input placeholder="Search" />
             <Search style={{ color: "gray", fontSize: 16 }} />
           </SearchContainer>
         </Left>
